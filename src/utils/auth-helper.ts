cf0a66011a88e106f6b8d95541e0fec0fc51600e
taraphirun <phiruntara@gtech.software>
2020-11-20 11:27:40 +0700

    - got frontend infinite scroll

--- src/utils/auth-helper.ts
+++ src/utils/auth-helper.ts
@@ -4,10 +4,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export const get_user_from_token = async (token: string) => {
   console.log("geting");
-  const data = verify(token, jwt_access_token_secret);
-  const isValidToken = await prisma.blacklist.findOne({
-    where: { token: token },
-  });
-  if (isValidToken === null) return data;
-  else return null;
+  try {
+    const data = verify(token, jwt_access_token_secret);
+    const isValidToken = await prisma.blacklist.findOne({
+      where: { token: token },
+    });
+    if (isValidToken === null) return data;
+    else return null;
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return null;
+    }
+  }
 };
